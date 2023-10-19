ee7113f5b754f2f07b022669846cf2c8a8952431
Fernanda Marques <fermarquessantos@gmail.com>
2023-10-18 23:45:20 -0300

    Initial commit

---
+++ src/main/java/desafio/dio/sb/Application.java
@@ -0,0 +1,16 @@
+package desafio.dio.sb;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server Url")})
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
